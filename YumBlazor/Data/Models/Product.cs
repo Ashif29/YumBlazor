e1d87ff43531005ad710d83361c5b6a3b4a7b547
Ashif Iqubal <asif.duet.cse19@gmail.com>
2024-08-22 15:17:58 +0600

    Added Product Model

---
+++ YumBlazor/Data/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace YumBlazor.Data.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(1, 1000)]
+        public int Price { get; set; }
+        public string? Description { get; set; }
+        public string? SpecialTag { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+    }
+}
