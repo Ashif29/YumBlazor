289887581fd0f8664b7010f495efe3c5c027076f
Ashif Iqubal <asif.duet.cse19@gmail.com>
2024-08-25 12:02:15 +0600

    Added Product Component

---
+++ YumBlazor/wwwroot/js/SaveScroll.js
@@ -0,0 +1,11 @@
+﻿window.scrollFunctions = {
+    saveScrollPosition: function () {
+        return {
+            x: window.scrollX,
+            y: window.scrollY
+        };
+    },
+    restoreScrollPosition: function (position) {
+        window.scrollTo(position.x, position.y);
+    }
+};
